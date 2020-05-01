001356c609be5721fc9b9c1e12670fd5aa3cd1b6
blackfur <hj8558@163.com>
2020-05-02 07:14:33 +0800

    download apk to sdcard

--- app/src/main/java/sample/note/VerUptActivity.kt
+++ app/src/main/java/sample/note/VerUptActivity.kt
@@ -6,12 +6,19 @@ import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.gson.GsonBuilder
 import com.google.gson.JsonObject
 import lombok.SneakyThrows
+import okhttp3.Call
 import okhttp3.Request
+import okhttp3.Response
+import sample.note.Global.okHttpClient
+import java.io.BufferedInputStream
+import java.io.BufferedOutputStream
+import java.io.FileOutputStream
+import java.io.InputStream
+
 
 class VerUptActivity : AppCompatActivity() {
 
@@ -28,6 +35,7 @@ class VerUptActivity : AppCompatActivity() {
 
         log = findViewById<TextView>(R.id.log)
         button = findViewById(R.id.go)
+        button!!.setOnClickListener { downloadQuest().execute() }
 
         log!!.append("checking version from server...\n")
 
@@ -36,6 +44,65 @@ class VerUptActivity : AppCompatActivity() {
 
     private fun uilog(msg: String){
        runOnUiThread { log!!.append(msg) }
+    }
+
+    inner class downloadQuest: AsyncTask<Any?, Any?, Any?>(){
+        private val tag = "download quest"
+        private val url = prefix + REMOTE_APK_FILE
+        override fun doInBackground(vararg params: Any?): Any? {
+            // Create request builder.
+            var builder: Request.Builder = Request.Builder()
+            // Set url.
+            runOnUiThread { log!!.append("downloading from: $url\n") }
+            builder = builder.url(url)
+            // Create request object.
+            val request = builder.build()
+            // Get okhttp3.Call object.
+            val response = client.newCall(request).execute()
+
+            val respCode = response.code
+            val respMsg = response.message
+            //val respBody = response.body!!.string()
+            Log.d(tag, "Response code : $respCode")
+            Log.d(tag, "Response message : $respMsg")
+            //Log.d(tag, "Response body : $respBody")
+
+            if(respCode != 200){
+               runOnUiThread { log!!.append("$respCode: $respMsg\n") }
+                return null
+            }
+
+            // File output stream related object.
+            val fileOutputStream = FileOutputStream(LOCAL_APK_FILE)
+            val bufferedOutputStream = BufferedOutputStream(fileOutputStream)
+
+            // File input stream related object.
+            val inputStream: InputStream = response.body!!.byteStream()
+            val bufferedInputStream = BufferedInputStream(inputStream)
+
+            // Read data from input stream.
+            val dataBuf = ByteArray(1024)
+            var readLen: Int = bufferedInputStream.read(dataBuf)
+            var total = readLen
+
+            // If read data byte length bigger than -1.
+            while (readLen > -1) {
+                // Write buffer data to output stream.
+                bufferedOutputStream.write(dataBuf, 0, readLen)
+                // Read data again.
+                readLen = bufferedInputStream.read(dataBuf)
+                total += readLen
+                if(total%1048576 == 0) runOnUiThread { log!!.append("-> ${total/1048576} mb\n") }
+            }
+            // Close input stream.
+            bufferedInputStream.close()
+            // Flush and close output stream.
+            bufferedOutputStream.flush()
+            bufferedOutputStream.close()
+
+            runOnUiThread { log!!.append("download success: ${total/1048576} mb, $LOCAL_APK_FILE\n") }
+            return null
+        }
     }
 
     inner class CheckQuest : AsyncTask<Any?, Any?, Any?>() {
--- app/src/main/java/sample/note/define.kt
+++ app/src/main/java/sample/note/define.kt
@@ -2,4 +2,5 @@
 
 package sample.note
 
-const val FOO = "foo"
+const val REMOTE_APK_FILE= "dist/release/app-release.apk"
+const val LOCAL_APK_FILE = "/sdcard/Download/app-release.apk"
